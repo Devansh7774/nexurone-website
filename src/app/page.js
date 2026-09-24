0616a6958c035bdffc68fab1d6654d8bb7710b9b
Dev7774 <7774devansh@gmail.com>
2026-09-24 10:04:59 +0530

    added a script in a home page

--- src/app/page.js
+++ src/app/page.js
@@ -13,9 +13,37 @@ const BlogInsights = dynamic(() => import("@/components/home/BlogInsights"));
 
 export const metadata = auditPageMetadata("/");
 
+const localBusinessJsonLd = {
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  name: "Nexuron Technologies",
+  image: "https://nexurontechnologies.com/logo-dark-t-e1756917561911.png",
+  "@id": "",
+  url: "https://nexurontechnologies.com",
+  telephone: "+91-75-67576896",
+  priceRange: "$",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "F-70-71, Hanumant Plaza, nr. Vadvala Hanumandada Temple, Balapir",
+    addressLocality: "Kadi",
+    postalCode: "384440",
+    addressCountry: "IN",
+  },
+  sameAs: [
+    "https://www.facebook.com/nexuron.tech",
+    "https://www.instagram.com/nexuron.technologies/",
+    "https://ca.linkedin.com/company/nexuron-technologies",
+    "https://nexurontechnologies.com",
+  ],
+};
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen w-full">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessJsonLd) }}
+      />
       <Hero />
       <AboutUs />
       <Features />
